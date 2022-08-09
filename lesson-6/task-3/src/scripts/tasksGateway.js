5b66bfd4d76dab41de52ec882b925a730d0cc562
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-09 17:42:20 +0300

    not works

---
+++ lesson-6/task-3/src/scripts/tasksGateway.js
@@ -0,0 +1,41 @@
+// const baseUrl = 'https://crudcrud.com/api/c28d6298d7984ed3ae29967a894d5dbf';
+const baseUrl =
+  'https://crudcrud.com/api/dab3bb3acc8245358def6828b2b405b2/tasks';
+
+function mapTasks(tasks) {
+  return tasks.map(({ _id, ...rest }) => ({ ...rest, id: _id }));
+}
+
+function getTasksList() {
+  return fetch(baseUrl)
+    .then(response => response.json())
+    .then(tasks => mapTasks(tasks));
+}
+
+function creatTask(taskData) {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(taskData),
+  });
+}
+
+function updateTask(taskId, updatedTaskData) {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(updatedTaskData),
+  });
+}
+
+function deleteTask(taskId) {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'DELETE',
+  });
+}
+
+export { getTasksList, creatTask, updateTask, deleteTask };
