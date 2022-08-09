5c10997d40bbd7e1e705a372482ae04f6b965951
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-09 20:24:21 +0300

    not works

--- lesson-6/task-3/temp/scripts/updateTask.js
+++ lesson-6/task-3/temp/scripts/updateTask.js
@@ -15,12 +15,12 @@ function onToggleTask(event) {
   const isCheckbox = event.target.classList.contains('list-item__checkbox');
   if (!isCheckbox) return;
   const taskId = event.target.dataset.id;
-  const done = event.target.checked;
   const tasksList = (0, _storage.getItem)('tasksList');
   const {
     text,
     createDate
   } = tasksList.find(task => task.id === taskId);
+  const done = event.target.checked;
   const updatedTask = {
     text,
     createDate,
@@ -31,7 +31,31 @@ function onToggleTask(event) {
     (0, _storage.setItem)('tasksList', newTasksList);
     (0, _renderer.renderTasks)();
   });
-} // 1. Prepare data
+} // import { renderTasks } from './renderer.js';
+// import { getItem, setItem } from './storage.js';
+// import { updateTask, getTasksList } from './tasksGateway.js';
+// function onToggleTask(event) {
+//   const isCheckbox = event.target.classList.contains('list-item__checkbox');
+//   if (!isCheckbox) return;
+//   const taskId = event.target.dataset.id;
+//   const done = event.target.checked;
+//   const tasksList = getItem('tasksList');
+//   const { text, createDate } = tasksList.find(task => task.id === taskId);
+//   const updatedTask = {
+//     text,
+//     createDate,
+//     done,
+//     finishDate: done ? new Date().toISOString() : null,
+//   };
+//   updateTask(taskId, updatedTask)
+//     .then(() => getTasksList())
+//     .then(newTasksList => {
+//       setItem('tasksList', newTasksList);
+//       renderTasks();
+//     });
+// }
+// export { onToggleTask };
+// 1. Prepare data
 // 2. Update data in db (Data Base)
 // 3. Read new data from server
 // 4. Save new data to front-end storage
